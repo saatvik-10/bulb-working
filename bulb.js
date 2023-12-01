857238f3388359efb4bb44d66691a3ef4a279394
saatvik-10 <146160585+saatvik-10@users.noreply.github.com>
2023-12-02 01:33:10 +0530

    bulb.js

--- bulb.js
+++ bulb.js
@@ -5,11 +5,13 @@ btn.addEventListener('click', toggleBulb)
 function toggleBulb(_event){
     if(btn.textContent.includes('On')){
     bulb.src="./bulbOn.png"
-    btn.textContent="Turn Off" 
+    btn.textContent="Turn Off"
+    bulb.classList.add("glow")
     }
     else{
         bulb.src="./bulbOff.png"
         btn.textContent="Turn On"
+        bulb.classList.remove("glow")
     }
 }
 
